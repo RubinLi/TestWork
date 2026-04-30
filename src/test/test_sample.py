d76ee0d401f62b5c83c89f0eccf4561d21da87cb
Rox <110010001@gmail.com>
2026-04-30 16:16:30 +0300

    Разбор аргументов Сli

--- src/test/test_sample.py
+++
@@ -1,9 +0,0 @@
-from src.testWork.main import inc  # content of test_sample.py
-
-
-def test_answer():
-    assert inc(3) == 3
-
-
-def test_answer2():
-    assert inc(8) == 9
--- src/testWork/main.py
+++ src/testWork/main.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 import os
 
 
@@ -24,13 +25,41 @@ class Settings:
     ):
         self.workdir = os.path.dirname(os.path.abspath(__file__))
         self.log_level = log_level
-        self.log_file = self.workdir + "/" + log_file
+        if log_file:
+            self.log_file = self.workdir + "/" + log_file
+        else:
+            self.log_file = None
+
+
+def argparsess():
+    str_args_files_help: str = "Имена csv-файлов с метриками видео на YouTube (может быть несколько)"
+    str_args_report_help: str = "Тип отчёта, отвечает за логикy расчётов"
+    str_prog_info: str = "cli приложение для обработки csv-файлов с метриками видео на YouTube"
+
+    parser = argparse.ArgumentParser(
+        prog=str_prog_info)
+    parser.add_argument('-f',
+                        '--files',
+                        type=str,
+                        nargs='*',
+                        help=str_args_files_help)
+    parser.add_argument('-r', '--report',
+                        type=str,
+                        help=str_args_report_help,
+                        nargs='?')
+    args: argparse.Namespace = parser.parse_args()
+    str_debug_msg: str = f"Аргументы cli > files: {
+        args.files}, report: {args.report}"
+    logging.debug(msg=str_debug_msg)
+    return args.files, args.report
 
 
 def main():
-    setting = Settings(log_level=logging.INFO, log_file="testwork.log")
+    setting = Settings(log_level=logging.DEBUG,
+                       log_file=None)  # "testwork.log")
     logging.basicConfig(level=setting.log_level, filename=setting.log_file,
                         format="%(asctime)s %(levelname)s: %(message)s")
+    files, report = argparsess()
     logging.debug("A DEBUG Message")
     logging.info("An INFO")
     logging.warning("A WARNING")
