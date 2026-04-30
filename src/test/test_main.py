2acc79838571737717f16a3aa6e9812bf5d0fd5e
Rox <110010001@gmail.com>
2026-04-30 21:20:16 +0300

    Чтение csv файла

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,9 +1,13 @@
 from src.testWork.main import *  # content of test_sample.py
 from unittest.mock import patch
+
+import sys
+from pathlib import Path
+
 import pytest
 
 
-class Testargparse:
+class TestArgparse:
     def test_argparsess_only_arg_files(self, capsys):
         with patch("sys.argv", ["main", "--files", "sss", "1"]):
             files, report = argparsess()
@@ -14,11 +18,49 @@ class Testargparse:
             files, report = argparsess()
             assert files == None and report == "report_type"
 
-    def test_argparsess_error_many_values_report(self, capsys):
+    def test_error_argparses_many_values_report(self, capsys):
         with pytest.raises(SystemExit) as excinfo:
             with patch("sys.argv", ["main", "--repor", "report_type", "error"]):
                 argparsess()
         assert excinfo.value.code != 0
         terminal = capsys.readouterr()
         assert "error: unrecognized arguments" in terminal.err
-        # assert "the following arguments are required: --fdef test_argparsess_error_many_values_report(self):
+
+
+class TestCSVread:
+    # TODO: Проверка обработки исключений через логер, или проброс ошибок в стиле
+    # wrap.
+    DATA_DIR = Path(__file__).resolve().parent.parent / "files"
+    FIELD_TYPE = {'title': str, 'ctr': float, 'retention_rate': float}
+    FIELD_NO_FOUND = {'error_field': str,
+                      'ctr': float, 'retention_rate': float}
+    FIELD_TYPE_ERROR = {'title': int,
+                        'ctr': float, 'retention_rate': float}
+
+    def test_read_csc_file(self):
+        likes = '0'
+        list_of_video: list | None = read_csv_file(
+            file_patch=self.DATA_DIR / 'stats1.csv', fieldtype=self.FIELD_TYPE)
+        print(list_of_video)
+        if list_of_video:
+            for video_info in list_of_video:
+                if video_info['title'] == 'Секрет который скрывают тимлиды':
+                    likes: str = video_info['ctr']
+            assert likes == 25.0
+        else:
+            assert None == 25.0
+
+    def test_error_csv_files_nofound(self, capsys):
+        None_file = read_csv_file(
+            file_patch=self.DATA_DIR/'NotFound.csv', fieldtype=self.FIELD_TYPE)
+        assert None_file == None
+
+    def test_error_csv_field_nofound(self, capfd):
+        None_file = read_csv_file(
+            file_patch=self.DATA_DIR/'stats1.csv', fieldtype=self.FIELD_NO_FOUND)
+        assert None_file == None
+
+    def test_error_csv_field_type_conver(self, capfd):
+        None_file = read_csv_file(
+            file_patch=self.DATA_DIR/'stats1.csv', fieldtype=self.FIELD_TYPE_ERROR)
+        assert None_file == None
--- src/testWork/main.py
+++ src/testWork/main.py
@@ -14,15 +14,16 @@ class Settings:
     log_file : str
         Путь к файлу журналирования
     log_level : int
-        Уровень журналирования 
+        Уровень журналирования
     """
 
     def __init__(
         self,
         log_level=logging.INFO,
-        log_file="logfile.log",
+        log_file: str | None = "logfile.log",
 
     ):
+        self.log_file: str | None
         self.workdir = os.path.dirname(os.path.abspath(__file__))
         self.log_level = log_level
         if log_file:
@@ -31,7 +32,7 @@ class Settings:
             self.log_file = None
 
 
-def argparsess():
+def argparsess() -> tuple[dict | None, str | None]:
     str_args_files_help: str = "Имена csv-файлов с метриками видео на YouTube (может быть несколько)"
     str_args_report_help: str = "Тип отчёта, отвечает за логикy расчётов"
     str_prog_info: str = "cli приложение для обработки csv-файлов с метриками видео на YouTube"
@@ -54,26 +55,74 @@ def argparsess():
     return args.files, args.report
 
 
+def read_csv_file(file_patch, fieldtype) -> list[dict] | None:
+    # TODO: Реализовать более полную обвязку для преобразования CSV файла в словарь.
+    class CSVFieldNotFoundError(Exception):
+        """Запрошеное поле отсутствует в csv файле"""
+
+    import csv
+    list = []
+    missing = {}
+
+    def filter_and_type_set(line, fieldtype):
+        return {
+            k: (v if k == str else fieldtype[k](v))
+            for k, v in line.items()
+            if k in fieldtype
+        }
+
+    try:
+        with open(file=file_patch) as f:
+            dictreader = csv.DictReader(f)
+            next(dictreader)
+            for line in dictreader:
+                # checking_field(line, fieldtype)
+                missing = set(fieldtype) - set(line)
+                if missing:
+                    raise (CSVFieldNotFoundError)
+                list.append(filter_and_type_set(line,  fieldtype))
+        str_debug_read_csv: str = f"Данные из файла csv ({file_patch}): {list}"
+        str_info_read_csv: str = f"Читаю csv файл {file_patch}"
+        logging.info(msg=str_info_read_csv)
+        logging.debug(msg=str_debug_read_csv)
+        return list
+    except FileNotFoundError:
+        str_warning_not_found: str = f"csv файл не найден! по пути{
+            file_patch}"
+        logging.warning(msg=str_warning_not_found)
+        return None
+    except CSVFieldNotFoundError:
+        str_warning_not_found: str = f"csv поле {
+            missing} не найдено в файле {file_patch}"
+        logging.warning(msg=str_warning_not_found)
+        return None
+    except ValueError as errortext:
+        str_warning_valueerror: str = f"Ошибка преобразования поля в новый тип: {
+            errortext}"
+        logging.warning(msg=str_warning_valueerror)
+        return None
+
+
 def main():
-    setting = Settings(log_level=logging.DEBUG,
-                       log_file=None)  # "testwork.log")
-    logging.basicConfig(level=setting.log_level, filename=setting.log_file,
+    settings = Settings(log_level=logging.DEBUG,
+                        log_file=None)  # "testwork.log")
+    logging.basicConfig(level=settings.log_level, filename=settings.log_file,
                         format="%(asctime)s %(levelname)s: %(message)s")
-    files, report = argparsess()
+    files: dict | None
+    report: str | None
+    # files, report = argparsess()
+    fieldtype = {'title': int, 'ctr': float, 'retention_rate': float}
+    list_of_video = read_csv_file(
+        file_patch=settings.workdir + '/../files/stats1.csv',
+        fieldtype=fieldtype)
+    print(list_of_video)
+
     logging.debug("A DEBUG Message")
     logging.info("An INFO")
     logging.warning("A WARNING")
     logging.error("An ERROR")
     logging.critical("A message of CRITICAL severity")
-    inc(3)
     print("Hello from workmate-testwork!")
-
-
-def inc(x):
-    if x > 6:
-        return x + 1
-    else:
-        return x
 
 
 if __name__ == "__main__":
