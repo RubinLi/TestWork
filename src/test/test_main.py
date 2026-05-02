6b71cfa25e0997af2ed03cd740481bc4a057ff49
Rox <110010001@gmail.com>
2026-05-02 11:40:08 +0300

    Журналирование и документация

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,3 +1,4 @@
+# import pyglet
 from src.testWork.main import *  # content of test_sample.py
 from unittest.mock import patch
 
@@ -6,25 +7,54 @@ from pathlib import Path
 
 import pytest
 
+# TODO: Сформировать тестовые данные и вынести в фикстуры.
+
 
 class TestArgparse:
+    @pytest.mark.skipif(reason="новая логика")
     def test_argparsess_only_arg_files(self, capsys):
         with patch("sys.argv", ["main", "--files", "sss", "1"]):
             files, report = argparsess()
             assert files == ["sss", "1"] and report == None
 
+    @pytest.mark.skipif(reason="новая логика")
     def test_argparsess_only_arg_report(self, capsys):
         with patch("sys.argv", ["main", "--report", "report_type"]):
             files, report = argparsess()
             assert files == None and report == "report_type"
 
-    def test_error_argparses_many_values_report(self, capsys):
+    def test_argparses_values_report(self, capsys):
+        with patch("sys.argv", ["main",
+                                "--files", "f1",
+                                "--report", "Clickbate"]):
+            files, report = argparsess(["Clickbate"])
+            assert files == ["f1"] and report == ["Clickbate"]
+
+    def test_error_argparses_values_report(self, capsys):
+
         with pytest.raises(SystemExit) as excinfo:
-            with patch("sys.argv", ["main", "--repor", "report_type", "error"]):
-                argparsess()
+            with patch("sys.argv", ["main",
+                                    "--files", "f1",
+                                    "--report", "error"]):
+                argparsess(["Clickbate"])
         assert excinfo.value.code != 0
         terminal = capsys.readouterr()
-        assert "error: unrecognized arguments" in terminal.err
+        assert "(choose from " in terminal.err
+
+
+class Testfiles_exists():
+    DATA_DIR = Path(__file__).resolve().parent
+
+    def test_file_exist(self):
+        exist = checking_files_exists(
+            [self.DATA_DIR.joinpath("files", "stats1.csv"),
+             self.DATA_DIR.joinpath("files", "stats2.csv")])
+        assert True == exist
+
+    def test_error_no_file_exist(self):
+        exist = checking_files_exists(
+            [self.DATA_DIR.joinpath("files", "error.csv")])
+        assert False == exist
 
 
 class TestCSVread:
@@ -36,31 +66,57 @@ class TestCSVread:
                       'ctr': float, 'retention_rate': float}
     FIELD_TYPE_ERROR = {'title': int,
                         'ctr': float, 'retention_rate': float}
+    clickbate = Clickbait(above_ctr=12, below_retention_rate=40)
 
     def test_read_csc_file(self):
         likes = '0'
-        list_of_video: list | None = read_csv_file(
-            file_patch=self.DATA_DIR / 'stats1.csv', fieldtype=self.FIELD_TYPE)
+        list_of_video: list | None = self.clickbate._read_csv_file(
+            file_csv_patch=self.DATA_DIR / 'stats1.csv', fieldtype=self.FIELD_TYPE)
         print(list_of_video)
         if list_of_video:
             for video_info in list_of_video:
                 if video_info['title'] == 'Секрет который скрывают тимлиды':
                     likes: str = video_info['ctr']
-            assert likes == 25.0
-        else:
-            assert None == 25.0
+        assert likes == 25.0
 
     def test_error_csv_files_nofound(self, capsys):
-        None_file = read_csv_file(
-            file_patch=self.DATA_DIR/'NotFound.csv', fieldtype=self.FIELD_TYPE)
-        assert None_file == None
+        None_file = self.clickbate._read_csv_file(
+            file_csv_patch=self.DATA_DIR/'NotFound.csv', fieldtype=self.FIELD_TYPE)
+        assert None_file is None
 
     def test_error_csv_field_nofound(self, capfd):
-        None_file = read_csv_file(
-            file_patch=self.DATA_DIR/'stats1.csv', fieldtype=self.FIELD_NO_FOUND)
-        assert None_file == None
+        None_file = self.clickbate._read_csv_file(
+            file_csv_patch=self.DATA_DIR/'stats1.csv', fieldtype=self.FIELD_NO_FOUND)
+        assert None_file is None
 
     def test_error_csv_field_type_conver(self, capfd):
-        None_file = read_csv_file(
-            file_patch=self.DATA_DIR/'stats1.csv', fieldtype=self.FIELD_TYPE_ERROR)
-        assert None_file == None
+        None_file = self.clickbate._read_csv_file(
+            file_csv_patch=self.DATA_DIR/'stats1.csv', fieldtype=self.FIELD_TYPE_ERROR)
+        assert None_file is None
+
+
+class TestClicbatefiltersuccess():
+    data = [{'title': 'success', 'ctr': 13, 'retention_rate': 30},
+            {'title': 'fail crt', 'ctr': 9.5, 'retention_rate': 30.0},
+            {'title': 'fail rait', 'ctr': 13, 'retention_rate': 82.0}]
+    DATA_DIR = Path(__file__).resolve().parent.parent / "files"
+    clickbate = Clickbait(above_ctr=12, below_retention_rate=40)
+
+    def test_filter(self):
+        filtered = self.clickbate._filter(self.data)
+        assert 1 == len(filtered)
+        assert filtered[0]["title"] == "success"
+
+    def test_sorted(self):
+        sorted = self.clickbate._sorted_data_decres_csv(self.data)
+        assert sorted[0]['ctr'] == 13
+
+    def test_extract_from_files(self):
+        result = self.clickbate._extract_from_files(
+            [self.DATA_DIR/'stats1.csv', self.DATA_DIR/'stats2.csv']
+        )
+        find0 = list(filter(lambda d: "в 5 утра" in d.get("title"), result))
+        find1 = list(filter(lambda d: "4" in d.get("title"), result))
+
+        assert find0[0]['ctr'] == 17.0
+        assert find1[0]['ctr'] == 22.5
--- src/testWork/main.py
+++ src/testWork/main.py
@@ -1,6 +1,9 @@
 import logging
 import argparse
-import os
+from abc import ABC, abstractmethod
+from pathlib import Path
+import sys
+from tabulate import tabulate
 
 
 class Settings:
@@ -20,109 +23,383 @@ class Settings:
     def __init__(
         self,
         log_level=logging.INFO,
-        log_file: str | None = "logfile.log",
+        log_file: str | None = 'testwork.log',
 
     ):
-        self.log_file: str | None
-        self.workdir = os.path.dirname(os.path.abspath(__file__))
+        self.log_file: Path | None
+        self.workdir = Path(__file__).resolve().parent
         self.log_level = log_level
         if log_file:
-            self.log_file = self.workdir + "/" + log_file
+            self.log_file = self.workdir.joinpath(log_file)
         else:
             self.log_file = None
 
 
-def argparsess() -> tuple[dict | None, str | None]:
+def argparsess(logic) -> tuple[list, str]:
+    """
+    Разбирает аргументы комадной строки
+
+    Параметры
+    ---------
+    lodic : list
+        Список типов отчетов, содержащих логику расчетов.
+        используеться для ограничения ввода пользователя,
+        существующими типами отчетов.
+
+    Возвращаемое значение
+    ---------------------
+    [list,str] : tumple
+        Кортеж содержащий список файлов и тип отчёта.
+    """
     str_args_files_help: str = "Имена csv-файлов с метриками видео на YouTube (может быть несколько)"
     str_args_report_help: str = "Тип отчёта, отвечает за логикy расчётов"
-    str_prog_info: str = "cli приложение для обработки csv-файлов с метриками видео на YouTube"
+    str_prog_info: str = "cli приложение для обработки csv-файлов метрик YouTube видео"
 
     parser = argparse.ArgumentParser(
         prog=str_prog_info)
     parser.add_argument('-f',
                         '--files',
                         type=str,
-                        nargs='*',
+                        nargs='+',
+                        required=True,
                         help=str_args_files_help)
     parser.add_argument('-r', '--report',
                         type=str,
                         help=str_args_report_help,
-                        nargs='?')
+                        required=True,
+                        choices=sorted(logic),
+                        nargs=1)
     args: argparse.Namespace = parser.parse_args()
-    str_debug_msg: str = f"Аргументы cli > files: {
-        args.files}, report: {args.report}"
-    logging.debug(msg=str_debug_msg)
-    return args.files, args.report
+
+    str_info_argparse: str = f"Извлекаю аргументы CLI"
+    str_debug_argparse: str = str_info_argparse + (
+        f" -> Список файлов: {args.files}, Логика отчёта: {args.report[0]}")
+    logging.info(msg=str_info_argparse)
+    logging.debug(msg=str_debug_argparse)
+    return args.files, args.report[0]
 
 
-def read_csv_file(file_patch, fieldtype) -> list[dict] | None:
-    # TODO: Реализовать более полную обвязку для преобразования CSV файла в словарь.
-    class CSVFieldNotFoundError(Exception):
-        """Запрошеное поле отсутствует в csv файле"""
+def checking_files_exists(files):
+    """
+    Проверяет существование файлов
 
-    import csv
-    list = []
-    missing = {}
+    Параметры
+    ---------
+    files : list
+        Список путей к файлам, для проверки существования
 
-    def filter_and_type_set(line, fieldtype):
-        return {
-            k: (v if k == str else fieldtype[k](v))
-            for k, v in line.items()
-            if k in fieldtype
-        }
+    Возвращаемое значение
+    ---------------------
+    Bool
+        Возвращает False если хотя бы один из файлов не существует
+        Возвращает True  если все файлы существуют
+    """
+    missing_files = [f for f in files if not Path(f).exists()]
+    if missing_files:
+        for missing_file in missing_files:
+            print(f"Файлы отсутствует по пути {missing_file}")
+        return False
+    str_info_files_exist: str = f"Проверяю наличие файлов статистики"
+    str_debug_files_exist: str = str_info_files_exist + (
+        f" -> Файлы существуют: {files}")
+    logging.info(msg=str_info_files_exist)
+    logging.debug(msg=str_debug_files_exist)
+    return True
 
-    try:
-        with open(file=file_patch) as f:
-            dictreader = csv.DictReader(f)
-            next(dictreader)
-            for line in dictreader:
-                # checking_field(line, fieldtype)
-                missing = set(fieldtype) - set(line)
-                if missing:
-                    raise (CSVFieldNotFoundError)
-                list.append(filter_and_type_set(line,  fieldtype))
-        str_debug_read_csv: str = f"Данные из файла csv ({file_patch}): {list}"
-        str_info_read_csv: str = f"Читаю csv файл {file_patch}"
-        logging.info(msg=str_info_read_csv)
-        logging.debug(msg=str_debug_read_csv)
-        return list
-    except FileNotFoundError:
-        str_warning_not_found: str = f"csv файл не найден! по пути{
-            file_patch}"
-        logging.warning(msg=str_warning_not_found)
-        return None
-    except CSVFieldNotFoundError:
-        str_warning_not_found: str = f"csv поле {
-            missing} не найдено в файле {file_patch}"
-        logging.warning(msg=str_warning_not_found)
-        return None
-    except ValueError as errortext:
-        str_warning_valueerror: str = f"Ошибка преобразования поля в новый тип: {
-            errortext}"
-        logging.warning(msg=str_warning_valueerror)
-        return None
+
+class Report():
+    """
+    Класс представляет интерфейс для работы с логикой отчетов.
+
+    Атрибуты
+    ---------
+    logic : Logic
+        Класс представляющий логику генерации отчетов
+    data: list
+        Обработанные в рамках генерации отчета данные.
+
+    """
+
+    def __init__(self, logic) -> None:
+        self._logic = logic
+        self._data = []
+
+    def report_data_from_files(self, files: list):
+        self._data = self._logic.report_from_files(files)
+        return self
+
+    def report_table(self) -> str:
+        return self._logic.report_table(self._data)
+
+    def report_text(self) -> None:
+        print("функция ещё не реализована")
+        pass
+
+
+class Logic(ABC):
+
+    @abstractmethod
+    def report_from_files(self, files) -> list:
+        pass
+
+    @abstractmethod
+    def report_table(self, data) -> str:
+        pass
+
+
+class Clickbait(Logic):
+    """
+    Класс работающий с информацией о видео  привлекающих внимание
+
+    ...
+
+    Атрибуты
+    --------
+    fieldtype : dict
+        словарь хранящий список полей для оценки
+        привлекательности видео, и соответствующие функции
+        для приведения значений полей к типу, подходящему для oценки.
+        Пример:
+
+        fieldtype = {'title': int, 'ctr': float, 'retention_rate': float}
+
+    above_ctr: float
+            нижнее значение кликабельности
+    below_retention_rate: float
+            Верхнее значение удержания
+    """
+
+    def __init__(self, above_ctr=15.0, below_retention_rate=40.0) -> None:
+        self.fieldtype: dict = {
+            'title': str,
+            'ctr': float,
+            'retention_rate': float}
+
+        self.above_ctr: float = above_ctr
+        self.below_retention_rate: float = below_retention_rate
+
+    def _extract_from_files(self, files: list) -> list:
+        """
+        Извлекает данные для анализа привлекательности видео
+        из csv файлов и возвращает список объединённых  данных
+        из нескольких файлов
+
+        Параметры
+        ---------
+        additional : list
+            список путей к csv файлам, для извлечения данных
+
+        Возвращаемое значение
+        ---------------------
+        union_list_of_video : list
+            список объединённых данных для анализа привлекательности
+        """
+        # TODO: если часть файлов не обработана продолжить работу опционально
+        union_list_of_video: list = []
+        for file in files:
+            list_of_video: list | None = self._read_csv_file(
+                file_csv_patch=file,
+                fieldtype=self.fieldtype)
+            if list_of_video:
+                union_list_of_video: list = union_list_of_video + list_of_video
+
+        str_info_filter: str = f"Записи извлечены из файлов: {files}"
+        str_debug_filter: str = str_info_filter + f" -> {union_list_of_video}"
+        logging.info(msg=str_info_filter)
+        logging.debug(msg=str_debug_filter)
+        return union_list_of_video
+
+    def _filter(self, data) -> list:
+        """
+        Выбирает информацию о видео
+        ,удовлетворяющих критериям привлекательности,
+        и возвращает в виде списка
+
+        Параметры
+        ---------
+        additional : list
+            список путей к csv файлам, для извлечения данных
+
+        Возвращаемое значение
+        ---------------------
+        fields : List
+            Список отфильтрованных данных
+        """
+        filtered = [line for line in data
+                    if line["ctr"] > self.above_ctr and
+                    line["retention_rate"] < self.below_retention_rate
+                    ]
+
+        str_info_filter: str = (f"Фильтрую записи огласно условию: "
+                                f" ctr > {self.above_ctr} retrntion_rate < {self.below_retention_rate}")
+        str_debug_filter: str = str_info_filter + f" -> {filtered}"
+        logging.info(msg=str_info_filter)
+        logging.debug(msg=str_debug_filter)
+        return filtered
+
+    def _sorted_data_decres_csv(self, data: list) -> list:
+        """
+        Cортирует информацию о видео по убыванию csv:
+
+        Параметры
+        ---------
+        data: list
+            список строк для сортировки
+
+        sorted_by_csv_desc : List
+            Сортированый список
+        """
+        sorted_by_csv_desc = sorted(
+            data, key=lambda d: d.get("ctr"), reverse=True)
+
+        str_info_sorted: str = "Сортирую записи по убыванию ctr: "
+        str_debug_sorted: str = str_info_sorted + f" -> {sorted_by_csv_desc}"
+        logging.info(msg=str_info_sorted)
+        logging.debug(msg=str_debug_sorted)
+        return sorted_by_csv_desc
+
+    def report_from_files(self, files: list):
+        """
+        Подготавливает данные из файлов для генерации отчета
+
+        Параметры
+        ---------
+        data: files
+            список путей к файлам.
+
+        final_date : List
+            подготовленные данные
+        """
+        files = self._extract_from_files(files)
+        filter_date = self._filter(files)
+        final_date = self._sorted_data_decres_csv(filter_date)
+        str_info_report: str = "Заканчиваю подготовку данных: "
+        str_debug_report: str = str_info_report + f" -> {filter_date}"
+        logging.info(msg=str_info_report)
+        logging.debug(msg=str_debug_report)
+        return final_date
+
+    def report_table(self, data) -> str:
+        """
+        Подготавливает данные из файлов для генерации отчета
+
+        Параметры
+        ---------
+        data: files
+            список путей к файлам.
+
+        final_date : List
+            подготовленные данные
+        """
+        table = tabulate(data,
+                         headers='keys',
+                         tablefmt="grid",
+                         colalign=("left", "center", "right"),
+                         missingval="N/A")
+        str_info_table: str = "Подготавливаю отчет в виде таблицы: "
+        str_debug_table: str = str_info_table + f" -> {table}"
+        logging.info(msg=str_info_table)
+        logging.debug(msg=str_debug_table)
+        return table
+
+    def _read_csv_file(self, file_csv_patch, fieldtype) -> list[dict] | None:
+        """
+        Читает из csv файла набор полей указанных в параметре fieldtype
+        и преобразует к необходимым типам.
+
+        Параметры
+        ---------
+        file_patch : str
+
+            Путь к файлу
+
+        fieldtype : dict
+
+            Словарь содержит набор полей для считывания из csv файла.
+            и соответствующие функции для преобразования типов.
+
+        Возвращаемое значение
+        ---------------------
+        list | none
+            Возвращает список словарей, 
+            каждый словарь
+            представляет собой строку csv файла.
+
+            В случаею ошибки чтения none.
+
+        """
+        # TODO: Реализовать более строгую обвязку для
+        # преобразования CSV файла в словарь.
+        class CSVFieldNotFoundError(Exception):
+            """Запрошеное поле отсутствует в csv файле"""
+        import csv
+        list = []
+        missing = {}
+
+        def filter_and_type_set(line, fieldtype):
+            return {
+                k: (v if k is str else fieldtype[k](v))
+                for k, v in line.items()
+                if k in fieldtype
+            }
+        try:
+            with open(file=file_csv_patch) as f:
+                dictreader = csv.DictReader(f)
+                # next(dictreader)
+                for line in dictreader:
+                    # checking_field(line, fieldtype)
+                    missing = set(fieldtype) - set(line)
+                    if missing:
+                        raise (CSVFieldNotFoundError)
+                    list.append(filter_and_type_set(line,  fieldtype))
+            str_debug_read_csv: str = (
+                f"Данные из файла csv ({file_csv_patch}): {list}")
+            str_info_read_csv: str = f"Читаю csv файл {file_csv_patch}"
+            logging.info(msg=str_info_read_csv)
+            logging.debug(msg=str_debug_read_csv)
+            return list
+        except FileNotFoundError:
+            str_warning_not_found: str = f"csv файл не найден! по пути{
+                file_csv_patch}"
+            logging.warning(msg=str_warning_not_found)
+            return None
+        except CSVFieldNotFoundError:
+            str_warning_not_found: str = f"csv поле {
+                missing} не найдено в файле {file_csv_patch}"
+            logging.warning(msg=str_warning_not_found)
+            return None
+        except ValueError as errortext:
+            str_warning_valueerror: str = (
+                f"Ошибка преобразования поля в новый тип: {
+                    errortext}")
+            logging.warning(msg=str_warning_valueerror)
+            return None
 
 
 def main():
-    settings = Settings(log_level=logging.DEBUG,
-                        log_file=None)  # "testwork.log")
+    input_files: list
+    selected_logic: str
+    settings = Settings(log_level=logging.INFO, log_file='newlogfile.log')
     logging.basicConfig(level=settings.log_level, filename=settings.log_file,
-                        format="%(asctime)s %(levelname)s: %(message)s")
-    files: dict | None
-    report: str | None
-    # files, report = argparsess()
-    fieldtype = {'title': int, 'ctr': float, 'retention_rate': float}
-    list_of_video = read_csv_file(
-        file_patch=settings.workdir + '/../files/stats1.csv',
-        fieldtype=fieldtype)
-    print(list_of_video)
+                        format="%(asctime)s %(funcName)s %(levelname)s: %(message)s")
+    logic_dict: dict = {
+        "Clickbait": Clickbait(above_ctr=12, below_retention_rate=40),
+        "Clickbait1": Clickbait(above_ctr=24, below_retention_rate=30)
+    }
+    logging.info(msg="Запуск")
 
-    logging.debug("A DEBUG Message")
-    logging.info("An INFO")
-    logging.warning("A WARNING")
-    logging.error("An ERROR")
-    logging.critical("A message of CRITICAL severity")
-    print("Hello from workmate-testwork!")
+    input_files, selected_logic = argparsess(logic=logic_dict.keys())
+    # print(input_files)
+    if not checking_files_exists(files=input_files):
+        print("Утилита завершает работу!")
+        sys.exit(1)
+
+    report_table: str = (Report(logic=logic_dict[selected_logic])
+                         .report_data_from_files(files=input_files)
+                         .report_table())
+
+    print("Таблица: Кликбейтные видео:")
+    print(report_table)
 
 
 if __name__ == "__main__":
